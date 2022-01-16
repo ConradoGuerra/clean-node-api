bdfa8db125fe18b1eb34fed56174cc70971cfded
Conrado Guerra <conradoguerra@gmail.com>
2022-01-16 00:25:49 -0300

    ensure userRouter to return a custom message of the error

--- src/presentation/routers/user-router.spec.js
+++ src/presentation/routers/user-router.spec.js
@@ -1,19 +1,41 @@
 class userRouter {
   route(httpRequest) {
     if (!httpRequest || !httpRequest.body) {
-      return {
-        statusCode: 500,
-      };
+      return httpResponse.serverError();
     }
     const { name, gender, city, birthday, age } = httpRequest.body;
-    if (!name || !gender || !city || !birthday || !age) {
-      return { statusCode: 400 };
+    if (!name) {
+      return httpResponse.badRequest("name");
+    } else if (!gender) {
+      return httpResponse.badRequest("gender");
+    } else if (!city) {
+      return httpResponse.badRequest("city");
+    } else if (!birthday) {
+      return httpResponse.badRequest("birthday");
+    } else if (!age) {
+      return httpResponse.badRequest("age");
     }
   }
 }
 
+class httpResponse {
+  static badRequest(paramName) {
+    return { statusCode: 400, body: new MissingParamError(paramName) };
+  }
+  static serverError() {
+    return { statusCode: 500 };
+  }
+}
+
+class MissingParamError extends Error {
+  constructor(paramName) {
+    super(`Missing param: ${paramName}`);
+    this.name = "MissingParamError";
+  }
+}
+
 describe("User Router", () => {
-  it("Should return statusCode 400 if no name is provided", () => {
+  it("Should return statusCode 400 and message if no name is provided", () => {
     const sut = new userRouter();
     const httpRequest = {
       body: {
@@ -25,9 +47,10 @@ describe("User Router", () => {
     };
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("name"));
   });
 
-  it("Should return statusCode 400 if no gender is provided", () => {
+  it("Should return statusCode 400 and message if no gender is provided", () => {
     const sut = new userRouter();
     const httpRequest = {
       body: {
@@ -39,9 +62,10 @@ describe("User Router", () => {
     };
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("gender"));
   });
 
-  it("Should return statusCode 400 if no city is provided", () => {
+  it("Should return statusCode 400 and message if no city is provided", () => {
     const sut = new userRouter();
     const httpRequest = {
       body: {
@@ -53,9 +77,10 @@ describe("User Router", () => {
     };
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("city"));
   });
 
-  it("Should return statusCode 400 if no birthday is provided", () => {
+  it("Should return statusCode 400 and message if no birthday is provided", () => {
     const sut = new userRouter();
     const httpRequest = {
       body: {
@@ -67,9 +92,10 @@ describe("User Router", () => {
     };
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("birthday"));
   });
 
-  it("Should return statusCode 400 if no age is provided", () => {
+  it("Should return statusCode 400 and message if no age is provided", () => {
     const sut = new userRouter();
     const httpRequest = {
       body: {
@@ -81,6 +107,7 @@ describe("User Router", () => {
     };
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("age"));
   });
 
   it("Should return statusCode 500 if no httpRequest is provided", () => {
